3491310fb425ee79f50d948cd84e96163f5e1b8f
Hasan Juraev <h.juraev@pflb.ru>
2023-08-13 12:57:41 +0300

    исправлено - некорректно прикладывались тела запроса и ответа (не менялись)

--- src/main/java/utils/Converter.java
+++ src/main/java/utils/Converter.java
@@ -7,12 +7,12 @@ public class Converter {
     public static String prettyPrintJson(String json) {
         JSONObject jsonObject = new JSONObject(json);
         String formattedRequest = String.format("{\n" +
-                        "  \"id\": 1,\n" +
-                        "  \"petId\": 505,\n" +
-                        "  \"quantity\": 1\n" +
-                        "  \"shipDate\" 2022-08-10T15:01:27.971Z\n" +
-                        "  \"status\" approved\n" +
-                        "  \"complete\" true\n" +
+                        "  \"id\": %s,\n" +
+                        "  \"petId\": %s,\n" +
+                        "  \"quantity\": %s,\n" +
+                        "  \"shipDate\": \"%s\",\n" +
+                        "  \"status\": \"%s\",\n" +
+                        "  \"complete\": %s\n" +
                         "}", jsonObject.get("id"), jsonObject.get("petId"), jsonObject.get("quantity"),
                 jsonObject.get("shipDate"), jsonObject.get("status"), jsonObject.get("complete"));
 
